bad3aaeba5cbbbdc87ce22bcb1a6697af2cb00f3
jr <jr@jirouette.com>
2018-03-04 22:32:14 +0100

    Now handling a full mock Duel until the opponent life points is 0

--- src/Main.hx
+++ src/Main.hx
@@ -1,5 +1,7 @@
 import overlay.Duel;
 import overlay.Player;
+import overlay.duels.Action;
+import overlay.cards.MonsterCard;
 
 import Random;
 
@@ -7,24 +9,82 @@ class Main
 {
 	static public function main()
 	{
+		var P1 = new Player("jr", "data/decks/random.deck");
+		var P2 = new Player("P2", "data/decks/random.deck");
 		var duel = new Duel();
-		duel.addPlayer(new Player("jr", "data/decks/random.deck"));
-		duel.addPlayer(new Player("P2", "data/decks/random.deck"));
+		duel.addPlayer(P1);
+		duel.addPlayer(P2);
 		duel.start();
 
-		var turn = duel.nextTurn();
-		var p = turn.turn_player;
-		trace("\n\n======  HAND OF "+p.name+"\n"+p.hand+"======\n\n");
-		trace("====== FIELD OF "+p.name+"\n"+p.field);
+		var player = if (duel.current_turn.turn_player == P1) P2 else P1;
+		var opponent = duel.current_turn.turn_player;
 
-		turn.action(p, CHANGE_PHASE(DRAW_PHASE));
-		turn.action(p, DRAW_CARD(1));
-		trace("\n\n====== HAND OF "+p.name+"\n"+p.hand+"======\n\n");
-		turn.action(p, CHANGE_PHASE(STANDBY_PHASE));
-		turn.action(p, CHANGE_PHASE(MAIN_PHASE));
-		var monster = p.hand.pop();
-		turn.action(p, NORMAL_SUMMON(monster, MONSTER_ZONE, Random.int(0,4)));
-		trace("====== FIELD OF "+p.name+"\n"+p.field);
-		turn.action(p, CHANGE_PHASE(END_PHASE));
+		var monster = cast(player.hand.last(), MonsterCard);
+
+		var actions = [
+
+			// opponent
+
+			CHANGE_PHASE(DRAW_PHASE),
+			DRAW_CARD(1),
+			CHANGE_PHASE(STANDBY_PHASE),
+			CHANGE_PHASE(MAIN_PHASE),
+			CHANGE_PHASE(END_PHASE),
+
+			// player
+
+			CHANGE_PHASE(DRAW_PHASE),
+			DRAW_CARD(1),
+			CHANGE_PHASE(STANDBY_PHASE),
+			CHANGE_PHASE(MAIN_PHASE),
+			NORMAL_SUMMON(monster, MONSTER_ZONE, Random.int(0,4)),
+			CHANGE_PHASE(BATTLE_PHASE),
+			DIRECT_ATTACK(monster, opponent),
+			CHANGE_PHASE(MAIN_PHASE),
+			CHANGE_PHASE(END_PHASE),
+		];
+
+		var loop_actions = [
+			// opponent
+
+			CHANGE_PHASE(DRAW_PHASE),
+			DRAW_CARD(1),
+			CHANGE_PHASE(STANDBY_PHASE),
+			CHANGE_PHASE(MAIN_PHASE),
+			CHANGE_PHASE(END_PHASE),
+
+			// player
+
+			CHANGE_PHASE(DRAW_PHASE),
+			DRAW_CARD(1),
+			CHANGE_PHASE(STANDBY_PHASE),
+			CHANGE_PHASE(MAIN_PHASE),
+			CHANGE_PHASE(BATTLE_PHASE),
+			DIRECT_ATTACK(monster, opponent),
+			CHANGE_PHASE(END_PHASE),
+		];
+
+		for(i in 0...20)
+			for(action in loop_actions)
+				actions.push(action);
+
+		var action = actions.shift();
+		while(duel.run(duel.current_turn.turn_player, action))
+		{
+			switch(action)
+			{
+				case NORMAL_SUMMON(m,z,i):
+					trace(duel.current_turn.turn_player.field);
+				case DRAW_CARD(n):
+					trace(duel.current_turn.turn_player.hand);
+				case CHANGE_PHASE(phase):
+					if (phase == END_PHASE)
+						trace("\n\n\n");
+				default:
+					// nothing to do
+			}
+
+			action = actions.shift();
+		}
 	}
 }
