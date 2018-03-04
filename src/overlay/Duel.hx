483fb9428c6ce136baf0cf2ad229244c29a3262c
jr <jr@jirouette.com>
2018-03-04 22:31:47 +0100

    Duel now has event loop method

--- src/overlay/Duel.hx
+++ src/overlay/Duel.hx
@@ -2,6 +2,7 @@ package overlay;
 
 import Random;
 import overlay.Player;
+import overlay.duels.Action;
 import overlay.duels.Turn;
 
 class Duel
@@ -9,6 +10,7 @@ class Duel
 	var players:Array<Player> = [];
 	var turns:List<Turn> = new List();
 	var nextTurnPlayer:Null<Player> = null;
+	public var current_turn:Null<Turn> = null;
 
 	static var MIN_DECK:Int = 40;
 	static var START_HAND:Int = 5;
@@ -28,6 +30,23 @@ class Duel
 				return false;
 			else
 				p.draw(START_HAND);
+		this.nextTurn();
+		return true;
+	}
+
+	public function run(player:Player, action:Action):Bool
+	{
+		this.current_turn.action(player, action);
+
+		for(p in players)
+			if (p.hasLost)
+			{
+				trace(p+": lost");
+				return false;
+			}
+
+		if (this.current_turn.current_phase == END_PHASE)
+			this.nextTurn();
 		return true;
 	}
 
@@ -43,6 +62,6 @@ class Duel
 		index = if (index+1 >= players.length) 0 else (index+1);
 		nextTurnPlayer = players[index];
 
-		return turn;
+		return this.current_turn = turn;
 	}
 }
