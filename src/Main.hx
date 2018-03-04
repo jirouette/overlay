60e0ad5cc83a7043cf281cddaf9c466f66c4e635
jr <jr@jirouette.com>
2018-03-04 21:39:55 +0100

    Main with recent update

--- src/Main.hx
+++ src/Main.hx
@@ -1,29 +1,21 @@
 import overlay.Duel;
 import overlay.Player;
-import overlay.duels.Turn;
-import overlay.duels.Action;
-import overlay.duels.Phase;
-import overlay.fields.Field;
-import overlay.fields.specifications.Position;
 
-import overlay.cards.specifications.Type;
-import overlay.cards.MonsterCard;
+import Random;
 
 class Main
 {
 	static public function main()
 	{
-		var p = new Player("jr");
-		p.deck.load("data/decks/random.deck");
-
 		var duel = new Duel();
-		duel.addPlayer(p);
+		duel.addPlayer(new Player("jr", "data/decks/random.deck"));
+		duel.addPlayer(new Player("P2", "data/decks/random.deck"));
 		duel.start();
 
-		var field = new Field();
-		var turn = new Turn(p);
+		var turn = duel.nextTurn();
+		var p = turn.turn_player;
 		trace("\n\n======  HAND OF "+p.name+"\n"+p.hand+"======\n\n");
-		trace("====== FIELD OF "+p.name+"\n"+field);
+		trace("====== FIELD OF "+p.name+"\n"+p.field);
 
 		turn.action(p, CHANGE_PHASE(DRAW_PHASE));
 		turn.action(p, DRAW_CARD(1));
@@ -31,12 +23,8 @@ class Main
 		turn.action(p, CHANGE_PHASE(STANDBY_PHASE));
 		turn.action(p, CHANGE_PHASE(MAIN_PHASE));
 		var monster = p.hand.pop();
-		turn.action(p, NORMAL_SUMMON(monster));
-
-		field.getZone(MONSTER_ZONE, 3).card = monster;
-		field.getZone(MONSTER_ZONE, 3).position = new Position(DEF_POSITION);
-
-		trace("====== FIELD OF "+p.name+"\n"+field);
+		turn.action(p, NORMAL_SUMMON(monster, MONSTER_ZONE, Random.int(0,4)));
+		trace("====== FIELD OF "+p.name+"\n"+p.field);
 		turn.action(p, CHANGE_PHASE(END_PHASE));
 	}
 }
